41732bfa6df1549656469b712e2fb68840f0ee9b
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-02-20 05:07:28 +0530

    Added header and image

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+   // let todaysDate=;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Image source={ nalvarImage } />
+      <Text style={{fontWeight: 'bold', color: 'red'}}>சிவாயநம</Text>
+      <Text style={{fontWeight: 'bold'}}>தினம் ஒரு திருமுறை</Text>
+        <Text>{ Date()}</Text>
+    
       </View>
     );
   }
 }
+export const nalvarImage = require('./img/nalvar.png'); 
 
 const styles = StyleSheet.create({
   container: {
