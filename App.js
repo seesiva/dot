8ab8f6d81f07370e3e9e6f7031d79b18697c5186
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-02-23 06:47:23 +0530

    Added JSON data and related alert

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
-
+import data from './dbstore/data.json'
 export default class App extends React.Component {
 
   render() {
@@ -12,12 +12,19 @@ export default class App extends React.Component {
       <Text style={{fontWeight: 'bold', color: 'green'}}> திருவளர்  பெருந்துறை  சிவோன்மத்தன்</Text>
       <Text style={{fontWeight: 'bold', color: 'green'}}> திருவடி போற்றி போற்றி {"\n\n"}</Text>
       <Image source={ nalvarImage } />
-      <Text style={{fontWeight: 'bold'}}>{"\n\n"}நால்வர் பொற்றாள்  எம் உயிர்த்துணையே </Text>
-      <Text style={{fontWeight: 'bold'}}>{"\n\n"}தினம் ஒரு திருமுறை{"\n\n"}</Text>
+      <Text style={{fontWeight: 'bold', color:'blue'}}>{"\n\n"}நால்வர் பொற்றாள்  எம் உயிர்த்துணையே </Text>
+      <Text style={{fontWeight: 'bold', color: 'grey'}}>{"\n\n"}தினம் ஒரு திருக்கோவையார்{"\n\n"}</Text>
        {/*<Text>{ Date()}</Text>*/}
         <Button
          onPress={() => {
-          Alert.alert('You tapped the button!');
+           let x=data.json;
+           var array = [];
+           for (let prop in data) {
+            // console.log(prop);
+            array.push(data[prop]);
+          }
+          console.log(data["songs"].length);
+          Alert.alert(data["songs"][3].song);
         }}
 title="இன்றைய திருமுறை"
   color="#841584"
